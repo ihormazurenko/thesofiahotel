efc4a20527c7aad311d387ba47966d366f42db6d
mib1092 <mazurenko.ihor@gmail.com>
2019-08-19 22:37:55 +0300

    fixes

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -264,21 +264,19 @@ jQuery(document).ready(function($) {
         if (typeof $.fn.magnificPopup !== 'undefined') {
             $(function () {
                 if ($('.popup-gallery a > img').length) {
-                    $('.popup-gallery').each(function() {
-                        $(this).magnificPopup({
-                            delegate: 'a',
-                            type: 'image',
-                            tLoading: 'Loading image #%curr%...',
-                            mainClass: 'mfp-img-mobile',
-                            gallery: {
-                                enabled: false,
-                                navigateByImgClick: true,
-                                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-                            },
-                            image: {
-                                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                            }
-                        });
+                    $('.popup-gallery').magnificPopup({
+                        delegate: 'a',
+                        type: 'image',
+                        tLoading: 'Loading image #%curr%...',
+                        mainClass: 'mfp-img-mobile',
+                        gallery: {
+                            enabled: false,
+                            navigateByImgClick: true,
+                            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+                        },
+                        image: {
+                            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                        }
                     });
                 }
 
